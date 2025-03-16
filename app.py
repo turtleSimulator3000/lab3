2c2f28e3014777c5d71d9b215dfdb17e350a1355
turtleSimulator3000 <121983433+turtleSimulator3000@users.noreply.github.com>
2025-03-15 21:42:02 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, send_file, redirect, jsonify
+import uuid
+app = Flask(__name__)
+from static.games.minesweeper.minesweeper import MineSweeper
+users = {
+    'help': 'me'
+}
+runningGames = dict()
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        mode = request.form.get("mode")
+        print('mode')
+        if mode == 'login':
+            if username in users and users[username] == password:
+                return 'Welcome ' + username
+            else: 
+                return 'Incorrect credentials'
+        elif mode == 'register':
+            users.update({username: password})
+            return 'Welcome new ' + username
+
+    else:
+        return send_file('static/login.html')
+
+@app.route("/games/minesweeper", methods = ['GET', 'POST'])
+def mine():
+    
+    return send_file('static/games/minesweeper/intro.html')
+
+@app.route("/games/minesweeper/game", methods = ['GET', 'POST'])
+def mineGame():
+    
+    return send_file('static/games/minesweeper/minesweeper.html')
+@app.errorhandler(404)
+def notFound(error):
+    return send_file('static/404.html'),404
+
+@app.route('/<game>/game', methods = ['GET','PUT','POST'])
+def indexGame(game):
+    if request.method == 'GET' and 'id' not in request.args: return send_file(f'static/games/{game}/intro.html')
+    if request.method == 'POST': 
+            if 'rows' not in request.args or 'cols' not in request.args or 'name' not in request.args: return "ERROR: missing arguments"
+            rows = int(request.form.get('rows'))
+            cols = int(request.form.get('cols'))
+            name = request.form.get('name')
+            Minegame = MineSweeper(rows,cols)
+            id = uuid.uuid1()
+            runningGames[str(id)] = Minegame
+            send_file(f'static/games/{game}/{game}.html')
+            return redirect(f'/{game}/game?id={id}')
+            return send_file(f'static/games/{game}/{game}.html')
+    if 'id' not in request.args:
+        return "Error: No game id"
+    ID = request.args.get('id')
+    if ID in runningGames.keys() :
+        if request.method == 'GET': return send_file(f'static/games/{game}/{game}.html')
+        
+        else:
+            currentGame = runningGames[ID]
+            jsonData = request.get_json()
+            if jsonData.get("action") == 'board':
+                boar = {
+
+                }
+                for i in range(currentGame.rows):
+                    for j in range(currentGame.cols):
+                        boar[(i,j)] = currentGame.getSpace(i,j)
+                return jsonify({"board": boar,"gameOver": currentGame.gameOver, "score": currentGame.score})
+            elif jsonData.get("action") == 'pick':
+                row = int(jsonData.get("row"))
+                col = int(jsonData.get("col"))
+                flag = jsonData.get("flag")
+                return jsonify({"space": currentGame.pickSpace(row,col,flag)})
+            elif jsonData.get("action") == 'space':
+                row = int(jsonData.get("row"))
+                col = int(jsonData.get("col"))
+                return jsonify({'space': currentGame.getSpace(row,col)})
+            elif jsonData.get("action") == 'name':
+                return jsonify({'name': currentGame.name})
+            elif jsonData.get("action") == 'score':
+                return jsonify({'score': currentGame.score})
+            elif jsonData.get("action") == 'time':
+                return jsonify({'score': currentGame.time})
+            
+           
+
+
+
+            
+
+    else:
+        print(runningGames.keys())
+        return "Error: invalid game id"
+"""
+@app.route('/<game>/game', methods = ['POST'])
+def indexGame(game):
+    rows = request.args.get('rows')
+    cols = request.args.get('cols')
+    name = request.args.get('name')
+    game = MineSweeper(rows,cols)
+    id = uuid.uuid()
+    runningGames[id] = game
+    redirect(f'/{game}/game?id = {id}')
+
+    
+    return send_file('static/games/' + game + "/game")
+"""  
+
+
+
+
+app.run(port=8080)
